97c308e029f74335bef57952273255eaceaf06ae
Anesu Simukai <ST10186719@rcconnect.edu.za>
2023-04-22 15:53:29 +0200

    Enter the number of ingredients

--- SimukaiRecipeApp/Program.cs
+++ SimukaiRecipeApp/Program.cs
@@ -7,10 +7,18 @@ using System.Threading.Tasks;
 namespace SimukaiRecipeApp
 {
     internal class Program
-    {
-        static void Main(string[] args)
+    { 
+        //Create an object for the UserPrompt external class to have access to the methods.
+        static UserPrompt User = new UserPrompt();
+        public static void Main(string[] args)
         {
-
+            Console.WriteLine(">>Console Recipe Application<<\n" +
+                                "_______________________________\n");
+            //Call the captureRecipedetails method
+            User.CaptureRecipeDetails();
+            
+            Console.ReadKey();
         }
+
     }
 }
--- SimukaiRecipeApp/UserPrompt.cs
+++ SimukaiRecipeApp/UserPrompt.cs
@@ -8,5 +8,16 @@ namespace SimukaiRecipeApp
 {
     internal class UserPrompt
     {
+
+        //Create a method that will capture all the recipe details.
+        public void CaptureRecipeDetails()
+        {
+            int numberOfIngredients;
+
+            //Prompt the user as to how many ingredients are to be entered.
+            Console.WriteLine("How many ingredients you want to enter:");
+            numberOfIngredients = Convert.ToInt32(Console.ReadLine());
+
+        }
     }
 }
