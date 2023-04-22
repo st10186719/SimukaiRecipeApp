a21e8d2199ca8c68ba725e5513028cf78f0d1a31
Anesu Simukai <ST10186719@rcconnect.edu.za>
2023-04-22 17:42:49 +0200

    Populate the automatic properties arrays

--- SimukaiRecipeApp/UserPrompt.cs
+++ SimukaiRecipeApp/UserPrompt.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace SimukaiRecipeApp
 {
-    internal class UserPrompt : Recipe
+    internal class UserPrompt
     {
 
-        int count;//A loop Count variable to keep track of each iteration of a loop. 
+        Recipe recipeObj = new Recipe();
+        int count;//A loop Count variable to keep track of each iteration of a loop.
 
         int[] arr_resetQuantity;
 
@@ -30,18 +31,19 @@ namespace SimukaiRecipeApp
         }
 
         //Create a method that will capture all the recipe details.
-        public void CaptureRecipeDetails()
+        public  void CaptureRecipeDetails()
         {
             int numberOfIngredients;
+            count = 1;
 
             //Prompt the user as to how many ingredients are to be entered.
             Console.WriteLine("How many ingredients you want to enter:");
             numberOfIngredients = Convert.ToInt32(Console.ReadLine());
 
             //Initialize the arrays and set a size to each of them
-            ingredientName = new string[numberOfIngredients];
-            ingredientQuantity = new int[numberOfIngredients];
-            measurement = new string[numberOfIngredients];
+            recipeObj.arr_IngredientName = new string[numberOfIngredients];
+            recipeObj.arr_Quantity = new int[numberOfIngredients];
+            recipeObj.arr_Measurements = new string[numberOfIngredients];
 
             //Capture all the ingredient details.
             for (int i = 0; i < numberOfIngredients; i++)
@@ -49,15 +51,17 @@ namespace SimukaiRecipeApp
 
                 Console.WriteLine($"\nIngredient {count}");
                 Console.Write("Enter the name: ");
-                ingredientName[i] = Console.ReadLine();
+                recipeObj.arr_IngredientName[i] = Console.ReadLine();
 
                 Console.Write("Enter the quantity: ");
-                ingredientQuantity[i] = Convert.ToInt32(Console.ReadLine());
+                recipeObj.arr_Quantity[i] = Convert.ToInt32(Console.ReadLine());
 
                 Console.Write("Enter the unit of measurement: ");
-                measurement[i] = Console.ReadLine();
+                recipeObj.arr_Measurements[i] = Console.ReadLine();
                 count++;
             }
+            
+
         }
 
         //Create a method that will capture all the recipe steps.
@@ -69,19 +73,20 @@ namespace SimukaiRecipeApp
             Console.Write("\nEnter the number of steps: ");
             numSteps = Convert.ToInt32(Console.ReadLine());
 
-            description = new string[numSteps];
+            recipeObj.arr_Description = new string[numSteps];
 
             //Capture all the descriptions for each step.
             for (int i = 0; i < numSteps; i++)
             {
                 Console.WriteLine($"Enter description of step : {count}");
-                description[i] = Console.ReadLine();
+                recipeObj.arr_Description[i] = Console.ReadLine();
                 count++;
             }
             Console.WriteLine("_____\n");
 
-
+            
         }
+
         //Create a method that will display all the details for the recipe.
         public void DisplayFullRecipe()
         {
@@ -90,13 +95,13 @@ namespace SimukaiRecipeApp
             Console.WriteLine("Details of Recipe [ingredients]\n".ToUpper());
 
             //Using a for loop, Read through the arrays for the display of all the details.
-            for (int i = 0; i < ingredientName.Length; i++)
+            for (int i = 0; i < recipeObj.arr_IngredientName.Length ; i++)
             {
                 Console.WriteLine(
-                    $"INGREDIENT {count}\n" +
-                    $"Name: {ingredientName[i].ToString()} \n" +
-                    $"Quantity: {ingredientQuantity[i].ToString()} \n" +
-                    $"Unit of measurement: {measurement[i].ToString()}\n");
+                    $"INGREDIENT {count}\n"+
+                    $"Name: {recipeObj.arr_IngredientName[i].ToString()} \n" +
+                    $"Quantity: {recipeObj.arr_Quantity[i].ToString()} \n" +
+                    $"Unit of measurement: {recipeObj.arr_Measurements[i].ToString()}\n");
                 count++;
             }
             Console.WriteLine("_____\n"); ;
@@ -106,13 +111,13 @@ namespace SimukaiRecipeApp
                 $"Description of each [step]\n".ToUpper());
 
             //Create a for each loop to capture all the descriptions for each step.
-            foreach (var desc in description)
+            foreach (var desc in recipeObj.arr_Description)
             {
                 Console.WriteLine($"Step {count}: {desc}");
                 count++;
             }
             Console.WriteLine("_____\n");
-
+            
         }
 
         //Create a method that will allow the user to scale the quantities.
@@ -122,19 +127,19 @@ namespace SimukaiRecipeApp
             double scale;
 
             //This array will hold the original values for the quantity array.
-            arr_resetQuantity = ingredientQuantity;
+            arr_resetQuantity = recipeObj.arr_Quantity;
 
             Console.WriteLine("Enter the factor in which your ingredient quantities are scaled \n" +
                     "E.g. scaled by a factor of 0.5 (half), 2 (double)\ror 3 (triple): ");
             scale = Convert.ToDouble(Console.ReadLine());//User input
 
             //Read through the quantity array, for further manipulation.
-            for (int i = 0; i < ingredientQuantity.Length; i++)
+            for (int i = 0; i < recipeObj.arr_Quantity.Length; i++)
             {
-                Console.WriteLine($"Scaled quantity for {ingredientName[i] + " = " + ingredientQuantity[i] * scale} \n");
+                Console.WriteLine($"Scaled quantity for {recipeObj.arr_IngredientName[i] +" = "+ recipeObj.arr_Quantity[i] * scale} \n");
 
             }
-
+            
         }
 
         //Create a method that is responsible for reseting all the quantity values to the original
@@ -152,7 +157,7 @@ namespace SimukaiRecipeApp
             {
                 case 1:
                     //Replace the values back to their original state.
-                    ingredientQuantity = arr_resetQuantity;
+                    recipeObj.arr_Quantity = arr_resetQuantity;
                     Console.WriteLine("The quantity values have been restored to their original values\n");
                     break;
                 case 2:
@@ -161,9 +166,9 @@ namespace SimukaiRecipeApp
                     Console.WriteLine("Invalid input");
                     break;
             }
-            for (int i = 0; i < ingredientQuantity.Length; i++)
+            for (int i = 0; i < recipeObj.arr_Quantity.Length; i++)
             {
-                Console.WriteLine($"Reset Quantity for {ingredientName[i] + " = " + ingredientQuantity[i]} \n");
+                Console.WriteLine($"Reset Quantity for {recipeObj.arr_IngredientName[i] + " = " + recipeObj.arr_Quantity[i]} \n");
 
             }
 
@@ -173,19 +178,19 @@ namespace SimukaiRecipeApp
         public void ClearData()
         {
             char response;
-
+            
             Console.WriteLine("Do you want to enter a new recipe? \nEnter (Y) to proceed or any other key to decline.");
             response = Convert.ToChar(Console.ReadLine());
 
             if (response.Equals('Y') || response.Equals('y'))
             {
-                for (int i = 0; i < ingredientName.Length; i++)
+                for (int i = 0; i < recipeObj.arr_IngredientName.Length; i++)
                 {
                     //Clear all the arrays, using the Array.Clear(Array arr, int index, array length) method.
-                    Array.Clear(ingredientName, 0, ingredientName.Length);
-                    Array.Clear(ingredientQuantity, 0, ingredientQuantity.Length);
-                    Array.Clear(measurement, 0, measurement.Length);
-                    Array.Clear(description, 0, description.Length);
+                    Array.Clear(recipeObj.arr_IngredientName, 0, recipeObj.arr_IngredientName.Length);
+                    Array.Clear(recipeObj.arr_Quantity, 0, recipeObj.arr_Quantity.Length);
+                    Array.Clear(recipeObj.arr_Measurements, 0, recipeObj.arr_Measurements.Length);
+                    Array.Clear(recipeObj.arr_Description, 0, recipeObj.arr_Description.Length);
                 }
                 Console.WriteLine("All the recipe details have been successfully cleared!\n");
 
