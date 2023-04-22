f8fdc94cca36b1fb83f358259aecd32ceca5648a
Anesu Simukai <ST10186719@rcconnect.edu.za>
2023-04-22 17:32:06 +0200

    Create automatic properties

--- SimukaiRecipeApp/Recipe.cs
+++ SimukaiRecipeApp/Recipe.cs
@@ -13,5 +13,27 @@ namespace SimukaiRecipeApp
         public int[] ingredientQuantity;//This variable holds the array of all ingredient quantities
         public string[] measurement;//This variable holds the array of all ingredient measurements
         public string[] description;//This variable holds the array of the descriptions of all the steps for a recipe.
+
+        public string[] arr_IngredientName
+        {
+            get { return ingredientName; }
+            set { ingredientName = value; }
+        }
+        public int[] arr_Quantity
+        {
+            get { return ingredientQuantity; }
+            set { ingredientQuantity = value; }
+        }
+        public string[] arr_Measurements
+        {
+            get { return measurement; }
+            set { measurement = value; }
+        }
+        public string[] arr_Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
     }
 }
